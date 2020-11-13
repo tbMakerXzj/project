b7add5df431c8a652526047ea304fd02a1ca7c6e
programmerxzj <1638492106@qq.com>
2020-11-13 16:33:10 +0800

    111

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+* 入口js文件
+* */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
---
+++ src/router/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  // 所有路由
+  routes: [
+    {
+      path: '/msite',
+      component: MSite, // 返回路由组件的函数, 只有执行此函数才会加载路由组件, 这个函数在请求对应的路由路径时才会执行
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login,
+      meta: {
+        showFooter: false
+      }
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+  ]
+})
